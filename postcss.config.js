bb500fe18652941d9c81169de7475ae2dc04ab6e
Slythefox18 <dcox6580@gmail.com>
2024-05-26 19:06:40 -0400

    Updated Configs

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: [
+      require('autoprefixer'),
+      require('postcss-nested')
+    ]
+  }
+  
+  module.exports = config
+
+  module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
